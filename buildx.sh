15f3ab17ee2f0dd2e0ab00d88528f88b19acd722
Mike Nye <mike.nye@gmail.com>
2020-06-11 00:03:53 +0800

    Prevent context changes during build

--- buildx.sh
+++ buildx.sh
@@ -10,8 +10,7 @@ docker buildx use homecluster
 
 # arm32v7 - build temp image to get versions
 echo "========== Building arm32v7 =========="
-docker context use arm32v7
-docker build -t "${REPO}/${IMAGE}:arm32v7_build" .
+docker --context=arm32v7 build -t "${REPO}/${IMAGE}:arm32v7_build" .
 FR24IMAGEVERSION=$(docker run --rm --entrypoint cat "${REPO}/${IMAGE}:arm32v7_build" /VERSION)
 echo "Tagging ${REPO}/${IMAGE}:${FR24IMAGEVERSION}"
 docker tag "${REPO}/${IMAGE}:arm32v7_build" "${REPO}/${IMAGE}:${FR24IMAGEVERSION}"
@@ -23,8 +22,7 @@ docker push "${REPO}/${IMAGE}:latest_armhf"
 
 # arm64 - build temp image to get versions
 echo "========== Building arm64 =========="
-docker context use arm64
-docker build -t "${REPO}/${IMAGE}:arm64_build" .
+docker --context=arm64 build -t "${REPO}/${IMAGE}:arm64_build" .
 FR24IMAGEVERSION=$(docker run --rm --entrypoint cat "${REPO}/${IMAGE}:arm64_build" /VERSION)
 echo "Tagging ${REPO}/${IMAGE}:${FR24IMAGEVERSION}"
 docker tag "${REPO}/${IMAGE}:arm64_build" "${REPO}/${IMAGE}:${FR24IMAGEVERSION}_arm64"
@@ -36,8 +34,7 @@ docker push "${REPO}/${IMAGE}:latest_arm64"
 
 # x86_64 - build temp image to get versions
 echo "========== Building x86_64 =========="
-docker context use x86_64
-docker build -t "${REPO}/${IMAGE}:x86_64_build" .
+docker --context=x86_64 build -t "${REPO}/${IMAGE}:x86_64_build" .
 FR24IMAGEVERSION=$(docker run --rm --entrypoint cat "${REPO}/${IMAGE}:x86_64_build" /VERSION)
 echo "Tagging ${REPO}/${IMAGE}:${FR24IMAGEVERSION}"
 docker tag "${REPO}/${IMAGE}:x86_64_build" "${REPO}/${IMAGE}:${FR24IMAGEVERSION}"
