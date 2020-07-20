695892e904af3f6ece70a8000c25b3e110908c0e
Mike Nye <mike.nye@gmail.com>
2020-07-20 14:05:55 +0800

    Linting (SC2086)

--- deploy_fr24feed.sh
+++ deploy_fr24feed.sh
@@ -15,7 +15,7 @@ FILEOUTPUT=$("${FILEBINARY}" -L "${FILEBINARY}")
 # Example output:
 # /usr/bin/file: ELF 32-bit LSB shared object, Intel 80386, version 1 (SYSV), dynamically linked, interpreter /lib/ld-musl-i386.so.1, stripped
 # /usr/bin/file: ELF 32-bit LSB shared object, Intel 80386, version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux.so.2, for GNU/Linux 3.2.0, BuildID[sha1]=d48e1d621e9b833b5d33ede3b4673535df181fe0, stripped  
-if echo ${FILEOUTPUT} | grep "Intel 80386" > /dev/null; then
+if echo "${FILEOUTPUT}" | grep "Intel 80386" > /dev/null; then
   FR24REPOPATH="linux_x86_binaries"
   FR24FEEDARCH="i386"
 fi
@@ -24,20 +24,20 @@ fi
 # Example output:
 # /usr/bin/file: ELF 64-bit LSB shared object, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib/ld-musl-x86_64.so.1, stripped
 # /usr/bin/file: ELF 64-bit LSB shared object, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, for GNU/Linux 3.2.0, BuildID[sha1]=6b0b86f64e36f977d088b3e7046f70a586dd60e7, stripped
-if echo ${FILEOUTPUT} | grep "x86-64" > /dev/null; then
+if echo "${FILEOUTPUT}" | grep "x86-64" > /dev/null; then
   FR24REPOPATH="linux_x86_64_binaries"
   FR24FEEDARCH="amd64"
 fi
 
 # armel
 # /usr/bin/file: ELF 32-bit LSB shared object, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux.so.3, for GNU/Linux 3.2.0, BuildID[sha1]=f57b617d0d6cd9d483dcf847b03614809e5cd8a9, stripped
-if echo ${FILEOUTPUT} | grep "ARM" > /dev/null; then
+if echo "${FILEOUTPUT}" | grep "ARM" > /dev/null; then
 
   # armhf
   # Example outputs:
   # /usr/bin/file: ELF 32-bit LSB shared object, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-musl-armhf.so.1, stripped  # /usr/bin/file: ELF 32-bit LSB shared object, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux-armhf.so.3, for GNU/Linux 3.2.0, BuildID[sha1]=921490a07eade98430e10735d69858e714113c56, stripped
   # /usr/bin/file: ELF 32-bit LSB shared object, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux-armhf.so.3, for GNU/Linux 3.2.0, BuildID[sha1]=921490a07eade98430e10735d69858e714113c56, stripped
-  if echo ${FILEOUTPUT} | grep "armhf" > /dev/null; then
+  if echo "${FILEOUTPUT}" | grep "armhf" > /dev/null; then
     FR24REPOPATH="rpi_binaries"
     FR24FEEDARCH="armhf"
   fi
@@ -46,7 +46,7 @@ if echo ${FILEOUTPUT} | grep "ARM" > /dev/null; then
   # Example output:
   # /usr/bin/file: ELF 64-bit LSB shared object, ARM aarch64, version 1 (SYSV), dynamically linked, interpreter /lib/ld-musl-aarch64.so.1, stripped
   # /usr/bin/file: ELF 64-bit LSB shared object, ARM aarch64, version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux-aarch64.so.1, for GNU/Linux 3.7.0, BuildID[sha1]=a8d6092fd49d8ec9e367ac9d451b3f55c7ae7a78, stripped
-  if echo ${FILEOUTPUT} | grep "aarch64" > /dev/null; then
+  if echo "${FILEOUTPUT}" | grep "aarch64" > /dev/null; then
     FR24REPOPATH="rpi_binaries"
     FR24FEEDARCH="armhf"
   fi
@@ -73,7 +73,7 @@ for FR24DEBFILE in $FR24DEBS
 do
 
     # Attempt .deb file download
-    if [ -z ${FR24FILEOVERRIDE+x} ]; then
+    if [ -z "${FR24FILEOVERRIDE+x}" ]; then
         echo "Attempting to download ${FR24DEBFILE}"
         curl --silent --output /tmp/fr24feed.deb "https://repo-feed.flightradar24.com/${FR24DEBFILE}"
         CURLEXITCODE="$?"
