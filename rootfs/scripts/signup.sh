fc50dc6dfe6c592cc67731eaf9263839430cc6f5
Mike <mike.nye@gmail.com>
2020-12-22 15:21:29 +0800

    work on expect script

--- rootfs/scripts/signup.sh
+++ rootfs/scripts/signup.sh
@@ -27,7 +27,9 @@ function write_fr24_expectscript() {
         echo 'expect "Step 1.3 - Would you like to participate in MLAT calculations? (yes/no)$:"'
         echo "send \"yes\r\""
         echo "expect \"Step 3.A - Enter antenna's latitude (DD.DDDD)\r\n\$:\""
+        # if [[ ${FEEDER_LAT:0:1} == "-" ]]; then
         echo "send \"${FEEDER_LAT}\r\""
+        
         echo "expect \"Step 3.B - Enter antenna's longitude (DDD.DDDD)\r\n\$:\""
         echo "send \"${FEEDER_LONG}\r\""
         echo "expect \"Step 3.C - Enter antenna's altitude above the sea level (in feet)\r\n\$:\""
