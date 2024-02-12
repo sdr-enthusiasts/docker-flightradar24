4e999b778ecc49c699d8f5c489dd039d97346296
kx1t <15090643+kx1t@users.noreply.github.com>
2024-02-12 14:12:50 -0500

    Update install_feeder.sh

--- install_feeder.sh
+++ install_feeder.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# This is a slightly updated version of FR24's install script that is available here:
-# 
+# This is a slightly updated version of FR24's install script
+#
 
-# the debian installer calls systemctl and udevadm. Let's make sure that doesn't fail
-echo '#/bin/bash' > /bin/systemctl
-echo '#/bin/bash' > /bin/udevadm
-chmod a+x /bin/systemctl /bin/udevadm
+# If $INSTALL_X86_NATIVE == "false" then we'll install the armhf build on x86 systems - the container will run that binary in qemu-arm-static instead of natively
+# This is done because for x86, the repo is stuck on a version 1.0.44 which is not compatible with the "new" UAT feeder code (which needs >=1.0.46-1)
+INSTALL_X86_NATIVE="${INSTALL_X86_NATIVE:-false}"
 
+# the debian installer calls systemctl and udevadm. Let's make sure that doesn't fail if they aren't present in a container
+# (Note we don't need them because we're using S6 to control system services, and the device drivers are already disabled in the host system)
+if ! which systemctl >/dev/null 2>&1; then
+    echo '#/bin/bash' > /bin/systemctl
+    chmod +x /bin/systemctl
+fi
+if ! which udevadm; then
+    echo '#/bin/bash' > /bin/udevadm
+    chmod +x /bin/udevadm
+fi
+
+# If this is run from a base container, we check if gpg exist, and if not we need to install a few packages:
 if ! gpg --version >/dev/null 2>&1; then
     apt update -y
     apt install -y --no-install-recommends gnupg binutils dirmngr
@@ -16,10 +27,10 @@ fi
 # to skip any questions from APT
 export DEBIAN_FRONTEND=noninteractive
 
-CHANNEL=stable
-SYSTEM=raspberrypi
+CHANNEL="stable"
+SYSTEM="raspberrypi"
 REPO="repo-feed.flightradar24.com"
-FEEDER=fr24feed
+FEEDER="fr24feed"
 
 ARCH_PATH=$(which arch || true)
 LSCPU_PATH=$(which lscpu || true)
@@ -47,23 +58,17 @@ case "$ARCH" in
 		;;
 esac
 
-# temp workaround to get the latest version of fr24feed also running in x86:
-case "$ARCH" in
-	x86_64|amd64)
+# If $INSTALL_X86_NATIVE == "false" then we'll install the armhf build on x86 systems - the container will run that binary in qemu-arm-static instead of natively
+# This is done because for x86, the repo is stuck on a version 1.0.44 which is not compatible with the "new" UAT feeder code (which needs >=1.0.46-1)
+if [[ "${INSTALL_X86_NATIVE,,} == "false" ]] && ( [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]] ); then
 	ARCH=arm64
 	SYSTEM="raspberrypi"
 	dpkg --add-architecture armhf
 	FEEDER="$FEEDER:armhf"
-	;;
-esac
-
-# already installed in Dockerfile, no need to do it twice
-# apt-get update -y
-# apt-get install dirmngr -y
+fi
 
 if [ ! -e "/etc/apt/keyrings" ]; then
-	mkdir /etc/apt/keyrings
-	chmod 0755 /etc/apt/keyrings
+	mkdir -p -m 0755 /etc/apt/keyrings
 fi
 
 # Import GPG key for the APT repository
@@ -79,9 +84,3 @@ apt-get install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force
 # # the debian installer calls systemctl and udevadm. Let's make sure that doesn't fail
 rm -f /bin/systemctl
 rm -f /bin/udevadm
-
-
-
-
-
-
