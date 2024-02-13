aeea37e52c9f7f63ad70dc0ed021b3f0a3ef6383
kx1t <kx1t@amsat.org>
2024-02-13 12:03:12 -0500

    remove separate logging; updated healthscript; other minor changes

--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -4,40 +4,19 @@ set -e
 
 EXITCODE=0
 
-# Check /var/log/fr24feed.log for errors in last 5 minutes
-LOG_ERROR_LAST_ENTRY=$(grep -a '\[e\]' /var/log/fr24feed.log | tail -1)
-LOG_ERROR_LAST_ENTRY_TIMESTAMP=$(date --date="$(echo "$LOG_ERROR_LAST_ENTRY" | cut -d ' ' -f 1,2)" +%s.%N)
-TIMESTAMP_NOW=$(date +%s.%N)
-RECENT_ERRORS_IN_LOG=$(echo "($TIMESTAMP_NOW" - "$LOG_ERROR_LAST_ENTRY_TIMESTAMP) < 300" | bc)
-if [ "$RECENT_ERRORS_IN_LOG" -eq 0 ]; then
-    echo "No recent errors in /var/log/fr24feed.log. HEALTHY"
-else
-    echo "Recent errors in /var/log/fr24feed.log: '${LOG_ERROR_LAST_ENTRY}'. UNHEALTHY"
-    EXITCODE=1
-fi
-
-# Logging is broken in fr24feeder v1.0.46-1 -- disabling this check for now
-# # Check /var/log/fr24feed.log for sent data in last 5 minutes
-# LOG_LAST_ENTRY=$(grep -a '\[feed\]\[i\]sent' /var/log/fr24feed.log | tail -1)
-# LOG_LAST_ENTRY_TIMESTAMP=$(date --date="$(echo "$LOG_LAST_ENTRY" | cut -d ' ' -f 1,2)" +%s.%N)
-# TIMESTAMP_NOW=$(date +%s.%N)
-# RECENT_LINE_IN_LOG=$(echo "($TIMESTAMP_NOW - $LOG_LAST_ENTRY_TIMESTAMP) < 300" | bc)
-# if [ "$RECENT_LINE_IN_LOG" -eq 1 ]; then
-#     echo "Data sent to fr24feed in past 5 mins. HEALTHY"
-# else
-#     echo "No data sent data to fr24feed in past 5 mins. UNHEALTHY"
-#     EXITCODE=1
-# fi
-
 # Check traffic over the last 5 minutes
+# Note - we are only checking ADSB traffic as there are generally messages even during low-traffic times
+#        UAT traffic is a lot more scarse and we won't check it to avoid false UNHEALTHY reports.
 TRAFFIC_FILE="${TRAFFIC_FILE:-/tmp/packets_rcvd}"
 if [[ -f "${TRAFFIC_FILE}" ]]; then
     read -ra traffic <<< "$(tail -1 "${TRAFFIC_FILE}")"
-    if (( $(date +%s) - traffic[0] > 600 )) || (( traffic[1] < 1 )); then
-        echo "No data received from $BEASTHOST in the past 5 mins. UNHEALTHY"
+    if (( $(date +%s) - traffic[0] > 600 )) || (( ${traffic[1]:-0} == 0 )); then
+        echo "[UNHEALTHY] No data received from $BEASTHOST in the past 5 mins"
         EXITCODE=1
+    elif (( traffic[1] == -1 )); then
+        echo "[WARNING] - Cannot check data flow, try adding NET_ADMIN and NET_RAW capabilities to your container"
     else
-        echo "${traffic[1]} packets received from $BEASTHOST in the past 5 mins. HEALTHY"
+        echo "[HEALTHY] ${traffic[1]} packets received from $BEASTHOST in the past 5 mins"
     fi
 fi
 
@@ -47,10 +26,20 @@ truncate -s 0 /var/log/fr24feed.log > /dev/null 2>&1
 
 # make sure we're listening on port 8754
 if netstat -an | grep LISTEN | grep 8754 > /dev/null; then
-    echo "listening for connections on port 8754. HEALTHY"
+    echo "[HEALTHY] ADSB status website is listening for connections on port 8754"
 else
-    echo "not listening for connections on port 8754. UNHEALTHY"
+    echo "[UNHEALTHY] ADSB status website is not listening for connections on port 8754"
     EXITCODE=1
+fi
+
+# make sure we're listening on port 8755 if UAT is enabled
+if [[ -n "$FR24KEY_UAT" ]]; then
+    if netstat -an | grep LISTEN | grep 8755 > /dev/null; then
+        echo "[HEALTHY] UAT status website is listening for connections on port 8755"
+    else
+        echo "[UNHEALTHY] UAT status website is not listening for connections on port 8755"
+        EXITCODE=1
+    fi
 fi
 
 # death count for fr24feed
@@ -59,44 +48,68 @@ SERVICENAME=$(basename "${SERVICEDIR}")
 # shellcheck disable=SC2126
 SERVICE_DEATHS=$(s6-svdt "${SERVICEDIR}" | grep -v "exitcode 0" | wc -l)
 if [ "$SERVICE_DEATHS" -ge 1 ]; then
-    echo "${SERVICENAME} error deaths: $SERVICE_DEATHS. UNHEALTHY"
+    echo "[UNHEALTHY] ${SERVICENAME} error deaths: $SERVICE_DEATHS"
     EXITCODE=1
 else
-    echo "${SERVICENAME} error deaths: $SERVICE_DEATHS. HEALTHY"
+    echo "[HEALTHY] ${SERVICENAME} error deaths: $SERVICE_DEATHS"
 fi
 s6-svdt-clear "${SERVICEDIR}"
 
-# death count for fr24feed_log
-SERVICEDIR=/run/service/fr24feed_log
-SERVICENAME=$(basename "${SERVICEDIR}")
-# shellcheck disable=SC2126
-SERVICE_DEATHS=$(s6-svdt "${SERVICEDIR}" | grep -v "exitcode 0" | wc -l)
-if [ "$SERVICE_DEATHS" -ge 1 ]; then
-    echo "${SERVICENAME} error deaths: $SERVICE_DEATHS. UNHEALTHY"
-    EXITCODE=1
-else
-    echo "${SERVICENAME} error deaths: $SERVICE_DEATHS. HEALTHY"
+# death count for fr24uat_feed
+if [[ -n "$FR24KEY_UAT" ]]; then
+    SERVICEDIR=/run/service/fr24uat_feed
+    SERVICENAME=$(basename "${SERVICEDIR}")
+    # shellcheck disable=SC2126
+    SERVICE_DEATHS=$(s6-svdt "${SERVICEDIR}" | grep -v "exitcode 0" | wc -l)
+    if [ "$SERVICE_DEATHS" -ge 1 ]; then
+        echo "[UNHEALTHY] ${SERVICENAME} error deaths: $SERVICE_DEATHS"
+        EXITCODE=1
+    else
+        echo "[HEALTHY] ${SERVICENAME} error deaths: $SERVICE_DEATHS"
+    fi
+    s6-svdt-clear "${SERVICEDIR}"
 fi
-s6-svdt-clear "${SERVICEDIR}"
 
-# the following checks are taken from /usr/local/bin/fr24feed-status
+# the following checks are taken from /usr/local/bin/fr24feed-status for ADSB
 MONITOR_FILE="/dev/shm/decoder.txt"
 # feed_status
 FEED_STATUS=$(grep "feed_status=" ${MONITOR_FILE} | cut -d= -f2)
 if [ "$FEED_STATUS" != 'connected' ]; then
-    echo "fr24 feed_status=${FEED_STATUS}. UNHEALTHY"
+    echo "[UNHEALTHY] fr24 ADSB feed_status=${FEED_STATUS}"
     grep "feed_status_message=" ${MONITOR_FILE}
     EXITCODE=1
 else
-    echo "fr24 feed_status=${FEED_STATUS}. HEALTHY"
+    echo "[UNHEALTHY] fr24 ADSB feed_status=${FEED_STATUS}"
 fi
 # rx_connected
 RX_CONNECTED=$(grep "rx_connected=" ${MONITOR_FILE} | cut -d= -f2)
 if [ "$RX_CONNECTED" != '1' ]; then
-    echo "fr24 rx_connected=${RX_CONNECTED}. UNHEALTHY"
+    echo "[UNHEALTHY] fr24 ADSB rx_connected=${RX_CONNECTED}"
     EXITCODE=1
 else
-    echo "fr24 rx_connected=${RX_CONNECTED} ($(grep num_messages= ${MONITOR_FILE} 2>/dev/null | cut -d'=' -f2) MSGS/$(grep num_resyncs= ${MONITOR_FILE} 2>/dev/null | cut -d'=' -f2) SYNC). HEALTHY"
+    echo "[HEALTHY] fr24 ADSB rx_connected=${RX_CONNECTED} ($(grep num_messages= ${MONITOR_FILE} 2>/dev/null | cut -d'=' -f2) MSGS/$(grep num_resyncs= ${MONITOR_FILE} 2>/dev/null | cut -d'=' -f2) SYNC)"
+fi
+
+# the following checks are taken from /usr/local/bin/fr24feed-status for UAT
+if [[ -n "$FR24KEY_UAT" ]]; then
+    MONITOR_FILE="/dev/shm/decoder.txt"
+    # feed_status
+    FEED_STATUS=$(grep "feed_status=" ${MONITOR_FILE} | cut -d= -f2)
+    if [ "$FEED_STATUS" != 'connected' ]; then
+        echo "[UNHEALTHY] fr24 UAT feed_status=${FEED_STATUS}"
+        grep "feed_status_message=" ${MONITOR_FILE}
+        EXITCODE=1
+    else
+        echo "[UNHEALTHY] fr24 UAT feed_status=${FEED_STATUS}"
+    fi
+    # rx_connected
+    RX_CONNECTED=$(grep "rx_connected=" ${MONITOR_FILE} | cut -d= -f2)
+    if [ "$RX_CONNECTED" != '1' ]; then
+        echo "[UNHEALTHY] fr24 UAT rx_connected=${RX_CONNECTED}"
+        EXITCODE=1
+    else
+        echo "[HEALTHY] fr24 UAT rx_connected=${RX_CONNECTED}"
+    fi
 fi
 
 exit $EXITCODE
