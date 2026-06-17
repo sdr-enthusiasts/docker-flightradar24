cd95d7b3b7d111dd23f4a63a3f9dfe14f59980b6
Delta / Subo /Stefan <delta_1977@icloud.com>
2026-06-17 13:27:01 +0200

    Update fr24feed package version to 1.0.57-1

--- install_feeder.sh
+++ install_feeder.sh
@@ -68,7 +68,7 @@ if [ ! -e "/etc/apt/keyrings" ]; then
 fi
 
 if [[ "${INSTALL_X86_FROMDEB,,}" == "true" ]] && { [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; }; then
-	curl -sSL https://repo-feed.flightradar24.com/linux_binaries/fr24feed_1.0.56-0_amd64.deb > fr24feed_1.0.56-0_amd64.deb
+	curl -sSL https://repo-feed.flightradar24.com/linux_binaries/fr24feed_1.0.57-1_amd64.deb > fr24feed_1.0.57-1_amd64.deb
 	dpkg -i fr24feed_1.0.56-0_amd64.deb
 else
 	# If $INSTALL_X86_NATIVE == "false" then we'll install the armhf build on x86 systems - the container will run that binary in qemu-arm-static instead of natively
