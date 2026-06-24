817a6db83ab8ff8d459cc0079249e7ebaebe84ae
Matthias Wirth <matthias.wirth@gmail.com>
2026-06-24 13:07:33 +0000

    remove unused scripts

--- get_adsb_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if /usr/bin/fr24feed --version >/dev/null 2>&1; then
-	exec /usr/bin/fr24feed --signup --configfile=/tmp/config.txt
-else
-	exec qemu-arm-static /usr/bin/fr24feed --signup --configfile=/tmp/config.txt
-fi
